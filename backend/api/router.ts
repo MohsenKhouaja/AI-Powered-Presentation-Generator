3ebfaf0e2a671772497374faba003619274f3a87
MohsenKhouaja <mohsenkhouaja3@gmail.com>
2026-04-29 22:35:37 +0100

    feat: add MongoDB support and enhance Docker commands in package.json

--- backend/api/router.ts
+++ backend/api/router.ts
@@ -3,12 +3,14 @@ import { contextsRouter } from "./contexts/router.js";
 import { filesRouter } from "./files/router.js";
 import { presentationsRouter } from "./presentations/router.js";
 import { presentationsAccessRouter } from "./presentations_access/router.js";
+import { slidesRouter } from "./slides/router.js";
 import { usersRouter } from "./users/router.js";
 
 export const apiRouter = Router();
 
 apiRouter.use(usersRouter);
 apiRouter.use(presentationsRouter);
+apiRouter.use(slidesRouter);
 apiRouter.use(contextsRouter);
 apiRouter.use(filesRouter);
 apiRouter.use(presentationsAccessRouter);
